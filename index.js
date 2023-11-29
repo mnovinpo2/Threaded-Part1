fe695093db4c0f14bf3ca9b7f25ff81cd26a6489
🙟O𒀱P🙝 <opringle9@gmaicom>
2023-11-29 15:55:42 -0800

    commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const path = require("path");
+const app = express();
+
+app.use(express.static(path.join(__dirname, "views"),{ extensions: ["html","htm"]}));
+app.use(express.static(path.join(__dirname,"public"),{ extensions: ["css","js"]}));
+app.use(express.static(path.join(__dirname,"media"),{ extensions: ["gif","jpg","png"]}));
+
+app.listen(8000, (err)=>{
+    if (err) throw err;
+    console.log("server started on port 8000");
+});
+
+app.get("/", (req, res) => {
+    res.send("index");
+
+});
+
+app.get("/contact", (req, res) => {
+    res.send("register");
+    
+});
+
+app.get("/register", (req, res) => {
+    res.send("register");
+});
